1ad9be90ec04b7ca0ad09f502b5b9442828443a8
Badrudin <Badrudin@Salaaxs-MacBook-Air.local>
2019-04-30 12:59:50 +0300

    ammend user class to include reset tokens

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from . import db
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from . import login_manager
+from . import db,login_manager
 from datetime import datetime
 from . import admin
 
@@ -18,14 +20,31 @@ class Quote:
         self.author = author
         self.quote = quote
 
-class User():
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
 
-    def __init__(self, username, email, bio, password):
 
-    self.username = username
-    self.email = email
-    self.bio = bio
-    self.password = password
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Pitch():
 
