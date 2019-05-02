aa606f516bed3ec08aebe7d34a456f465b85bd30
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-05-02 05:28:03 +0300

    modify class user

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class Quote:
         self.quote = quote
 
 class User(db.Model, UserMixin):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
