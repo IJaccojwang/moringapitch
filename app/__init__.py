c4f9ac01ee1ebbdc98359e7c25c5e03b2e55e835
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-05-02 05:23:56 +0300

    remove bootstrap and simplemde then add and initialize

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,42 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_simplemde import SimpleMDE
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from config import config_options
 from flask_mail import Mail
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
-    app = Flask(__name__)
+     app = Flask(__name__)
 
-    app.config.from_object(config_options[config_name])
+     app.config.from_object(config_options[config_name])
      #Initializing Flask Extensions
-    db.init_app(app)
-    login_manager.init_app(app)
-    mail.init_app(app)
+     bootstrap.init_app(app)
+     db.init_app(app)
+     login_manager.init_app(app)
+     mail.init_app(app)
+     simple.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+     from app.auth.views import auth
+     app.register_blueprint(auth)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+     from app.main.views import main
+     app.register_blueprint(main)
 
-    return app
+     # from .main import main as main_blueprint
+     # app.register_blueprint(main_blueprint)
+
+     # from .auth import auth as auth_blueprint
+     # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+     return app
