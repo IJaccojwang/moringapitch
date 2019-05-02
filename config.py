6c6bb9d863b9659332d846dc6a3b0f43569f74fc
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-05-02 05:31:56 +0300

    add mail username

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://salaaxnoor:123@localhost/moringapitches'
     
     #email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -12,6 +13,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
